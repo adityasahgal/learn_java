857e2d500e43ab1834a065a2149e03024d77e4bd
Aditya Sahgal <adityasahagal399@gmail.com>
2026-03-14 18:55:19 +0530

    Learn Methods in Java

---
+++ Day 4/Method.java
@@ -0,0 +1,10 @@
+public class Method {
+
+    static void greet() {
+        System.out.println("Hello Sahgal, Welcome to Java!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
