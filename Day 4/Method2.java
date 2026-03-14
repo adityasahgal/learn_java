1a3479a65fc56468967cdf22c03d9a77b1d97fb6
Aditya Sahgal <adityasahagal399@gmail.com>
2026-03-14 19:50:52 +0530

    Learn Methods with peremeter

---
+++ Day 4/Method2.java
@@ -0,0 +1,11 @@
+public class Method2 {
+
+    static void addNumbers(int a, int b, int c) {
+        int sum = a + b + c;
+        System.out.println("Sum: " + sum);
+    }
+
+    public static void main(String[] args) {
+        addNumbers(10, 20, 45);
+    }
+}
