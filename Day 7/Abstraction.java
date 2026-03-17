6a49bb61c776d6736b03c1afebaf32585c499d3c
Aditya Sahgal <adityasahagal399@gmail.com>
2026-03-17 19:46:56 +0530

    feat: practice abstraction method

--- Day 7/Abstraction.java
+++ Day 7/Abstraction.java
@@ -14,11 +14,22 @@ class Car extends Vehicle {
     }
 }
 
+class Bike extends Vehicle {
+
+    void start() {
+        System.out.println("Bike starts with key");
+    }
+}
+
 public class Abstraction {
     public static void main(String[] args) {
 
         Car c = new Car();
         c.start();
         c.fuelType();
+
+        Bike b = new Bike();
+        b.start();
+        b.fuelType();
     }
 }
