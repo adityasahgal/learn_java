9ccee61d90d0cfdd2a44ef25bd2a84b3a1c07ff8
Aditya Sahgal <adityasahagal399@gmail.com>
2026-04-01 23:02:58 +0530

    feat(Day 22): implement java learning task

---
+++ Day 22/day22auth/src/main/java/com/sahgal/day22auth/Day22authApplication.java
@@ -0,0 +1,13 @@
+package com.sahgal.day22auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day22authApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day22authApplication.class, args);
+	}
+
+}
